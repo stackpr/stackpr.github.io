6cfc1449b4e121076eada8b598814c4859e1242b
Ubuntu <ubuntu@ip-10-147-3-25.ec2.internal>
2015-01-02 15:55:39 -0600

    #1 - build-assets.php populates derivatives folder.

--- _bin/build-assets.php
+++ _bin/build-assets.php
@@ -4,14 +4,56 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
   echo "Web access is disabled.\n";
   exit;
 }
-
 chdir(dirname(__DIR__) . '/assets');
+
+// Extract the derivatives.
+$image_sizes = array();
 foreach (glob('derivatives/*') as $dir) {
   if (!is_dir($dir)) {
     continue;
   }
   $dims = basename($dir);
   if (preg_match('@^(\d+)x(\d+)$@s', $dims, $arr)) {
-    echo "$dims\n";
+    $image_sizes[] = $dims;
+  }
+}
+
+// Configure the optimizations.
+$optimizers = array(
+  '@\.jpe?g$@si' => 'jpegoptim --strip-all :path',
+  '@\.png$@si' => 'optipng -o7 :path',
+);
+
+// Iterate over the images.
+foreach (glob('files/*') as $path) {
+  if (!is_file($path) || !filesize($path)) {
+    continue;
+  }
+  $basename = basename($path);
+
+  // Skip non-images.
+  if (!preg_match('@\.(?:jpe?g|png|gif)$@si', $basename)) {
+    continue;
+  }
+
+  // Determine whether to convert an image.
+  foreach ($image_sizes as $size) {
+    if (is_file("derivatives/$size/$basename")) {
+      continue;
+    }
+    $basenameX = escapeshellarg($basename);
+    $cmd = "convert -resize $size files/$basenameX derivatives/$size/$basenameX";
+    echo "$cmd\n";
+    system($cmd);
+    foreach ($optimizers as $match => $optimizer) {
+      if (!preg_match($match, $basename)) {
+        continue;
+      }
+      $cmd = strtr($optimizer, array(
+        ':path' => "derivatives/$size/$basenameX",
+      ));
+      echo "  $cmd\n";
+      system($cmd);
+    }
   }
 }
